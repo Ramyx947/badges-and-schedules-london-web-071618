4421ace43a373a6b104b8314cf6d688b16c23a0f
Ramona M <r1mari@yahoo.com>
2018-07-05 14:45:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,7 @@
 # Write your code here.
-name=["Edsger" "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
 
 def batch_badge_creator(name)
+  name=["Edsger" "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   name.each {|name| puts "Hello, my name is #{name}."}
 end
 
