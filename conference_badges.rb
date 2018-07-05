ffc3a006462d2919846e14fceffd7fd7c6667da2
Ramona M <r1mari@yahoo.com>
2018-07-05 14:59:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,13 @@ def batch_badge_creator(array)
 end
   return new_array
 end
-def assign_rooms
+def assign_rooms(array)
+  new_array=[]
+  counter=1 
+  array.each do |name| new_array.push("Hello, #{name}! You'll be assign to room #{counter}!")
+  counter += 1
+end
+return new_array
 end
 
 def printer(batch_badge_creator, assign_rooms)
